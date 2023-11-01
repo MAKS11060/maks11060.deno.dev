89cd3e19fe71a7d6fb72419df9b8b09dbb7610c7
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2023-11-01 07:00:26 +0200

    check bcript

--- routes/api.ts
+++ routes/api.ts
@@ -1,5 +1,13 @@
-import { Hono } from "../deps.ts";
+import {Hono, bcrypt} from "../deps.ts"
 
 const app = new Hono()
 export {app as api}
 
+const salt = await bcrypt.genSalt(10)
+
+app.get('/b/test', async c => {
+  c.json({
+    salt,
+    hash: await bcrypt.hash(c.req.query('p') || '', salt)
+  })
+})
